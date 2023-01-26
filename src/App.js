a2e516383b3e8c4d9cfb1fd98fbd90e758beab5e
z(0o0a) <i7can7i@gmail.com>
2023-01-26 23:19:46 +0900

    Bootstrap에서 상단바 가져오기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Navbar, Container, Nav } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-
-      
+      <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="#home">z(0o0a)</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Sweet</Nav.Link>
+            <Nav.Link href="#pricing">Home</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
     </div>
   );
 }
