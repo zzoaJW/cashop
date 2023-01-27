8d21c1bf8b8ce8c3c30adf32ad6afc09762ec41c
z(0o0a) <i7can7i@gmail.com>
2023-01-27 23:03:45 +0900

    다른 파일에 있는 데이터 가져오기(export, import)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Container, Nav, Row, Col } from 'react-bootstrap';
+import { useState } from 'react';
+import data from './data.js';
+
 
 function App() {
+
+  let [furniture] = useState(data);
+
   return (
     <div className="App">
       <Navbar bg="light" variant="light">
@@ -21,17 +27,17 @@ function App() {
       <Container>
         <Row>
           <Col sm>
-            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="90%"/>
+            <img src="https://image.ohou.se/i/bucketplace-v2-development/uploads/productions/166009106474136296.jpg?gif=1&w=960&h=960&c=c&webp=1" width="100%"/>
+            <h4>{furniture[0].title}</h4>
+            <p>{furniture[0].content}</p>
+          </Col>
+          <Col sm>
+            <img src="https://image.ohou.se/i/bucketplace-v2-development/uploads/productions/164388564242609760.jpg?gif=1&w=960&h=960&c=c&webp=1" width="100%"/>
             <h4>상품명</h4>
             <p>상품설명</p>
           </Col>
           <Col sm>
-            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="90%"/>
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </Col>
-          <Col sm>
-            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="90%"/>
+            <img src="https://image.ohou.se/i/bucketplace-v2-development/uploads/productions/161554859365237314.jpg?gif=1&w=960&h=960&c=c&webp=1" width="100%"/>
             <h4>상품명</h4>
             <p>상품설명</p>
           </Col>
---
+++ src/data.js
@@ -0,0 +1,24 @@
+let data = [
+    {
+      id : 0,
+      title : "Modern Wooden Chair",
+      content : "Born in France",
+      price : 120000
+    },
+  
+    {
+      id : 1,
+      title : "Cozy Home Sofa",
+      content : "Born in Seoul",
+      price : 110000
+    },
+  
+    {
+      id : 2,
+      title : "Fancy Gold Chair",
+      content : "Born in the States",
+      price : 130000
+    }
+  ] 
+
+export default data;
