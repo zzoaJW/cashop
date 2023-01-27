054aadf601c9884aed50bd9ca657e8c623e9235f
z(0o0a) <i7can7i@gmail.com>
2023-01-28 00:31:54 +0900

    메인페이지, about페이지, detail페이지 라우팅

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Container, Nav, Row, Col } from 'react-bootstrap';
 import { useState } from 'react';
 import data from './data.js';
-
+import { Routes, Route, Link } from 'react-router-dom';
 
 function App() {
 
@@ -11,23 +11,39 @@ function App() {
 
   return (
     <div className="App">
+
       <Navbar bg="light" variant="light">
         <Container>
-          <Navbar.Brand href="#home">z(0o0a)</Navbar.Brand>
+          <Navbar.Brand href="/">z(0o0a)</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Sweet</Nav.Link>
-            <Nav.Link href="#pricing">Home</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/about">Sweet</Nav.Link>
+            <Nav.Link href="/detail">Home</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
 
+      <Routes>
+        <Route path='/' element={
+          <HomePage furnitures={furnitures}/>
+        }/>
+        <Route path='/about' element={<div>ㅇㅇㅇ</div>}/>
+        <Route path='/detail' element={<div>상세페이지임</div>}/>
+      </Routes>
+      
+    </div>
+  );
+}
+
+function HomePage(props){
+  return (
+    <>
       <div className='main-banner'/>
 
       <Container>
         <Row>
           {
-            furnitures.map(function(f, i){
+            props.furnitures.map(function(f, i){
               return (
                 <Card furnitures={f}/>
               )
@@ -35,8 +51,8 @@ function App() {
           }
         </Row>
       </Container>
-    </div>
-  );
+    </>
+  )
 }
 
 function Card(props){
