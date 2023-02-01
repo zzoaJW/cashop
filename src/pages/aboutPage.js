bb6e29c2c3c27c724331bd5bd59c57769b36ab28
z(0o0a) <i7can7i@gmail.com>
2023-02-02 01:58:04 +0900

    three.js 설치

--- src/pages/aboutPage.js
+++ src/pages/aboutPage.js
@@ -2,12 +2,9 @@ import { motion } from "framer-motion";
 
 function AboutPage(){
     return (
-        <motion.button
-        whileHover={{ scale: 1.1 }}
-        whileTap={{ scale: 0.9 }}
-        style={{width:"100px", height:"100px"}}
-      />
+        <></>
     )
 }
+
 
 export default AboutPage;
