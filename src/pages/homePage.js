698669b297b75df88ad1f527d2cf80d596563302
z(0o0a) <i7can7i@gmail.com>
2023-03-23 20:01:45 +0900

    서버 데이터 요청시 로딩 애니메이션, 데이터 result에서 필요한 만큼만 업데이트

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -3,6 +3,9 @@ import { Navbar, Container, Nav, Row, Col} from 'react-bootstrap';
 import axios from 'axios';
 
 function HomePage(props){
+  let [loading, setLoading] = useState(false)
+  let [loadFunNum, setLoadFunNum] = useState(0)
+
   return (
     <>
       <div className='main-banner'/>
@@ -19,18 +22,40 @@ function HomePage(props){
         </Row>
       </Container>
 
-      // 버튼 1번 클릭 : 3,4,5번째 상품 불러오기
-      // 버튼 2번 클릭 : 6,7,8번째 상품 불러오기
+      {
+        loading? 
+        <>
+          <br/>
+            <div className='loading'/>
+          <br/>
+        </> 
+        : null
+      }    
+      
+      {/* 
+      // 버튼 1번 클릭 : 3번째 상품 불러오기
+      // 버튼 2번 클릭 : 4번째 상품 불러오기
       // 버튼 3번 클릭 : 상품이 더 없다고 알려주기
+      // 공통 : 버튼 클릭시 "로딩중" 띄우기 
+      */}
+      <br/>
       <button onClick={ ()=>{
       axios.get('https://codingapple1.github.io/shop/data2.json')
-          .then((result)=>{ 
-            props.setFurnitures([...props.furnitures, ...result.data])
+          .then((result)=>{
+            setLoading(true)
+            props.setFurnitures([...props.furnitures, ...result.data.slice(loadFunNum, loadFunNum+1)])
+            setLoadFunNum(loadFunNum+1)
+            setLoading(false)
+            
+            {
+              return loadFunNum >= result.data.length? alert('마지막 상품입니다.') : null
+            }
           })
           .catch((e)=>{
+            setLoading(false)
             alert(e)
           })
-    } }>더보기</button>
+      } }>더보기</button>
 
     </>
   )
@@ -38,8 +63,8 @@ function HomePage(props){
 
   function Card(props){
     return (
-      <Col>
-        <img src={props.furnitures.img} width="80%" onClick={()=>{ props.navigate(`/detail/${props.id}`)}}/>
+      <Col onClick={()=>{ props.navigate(`/detail/${props.id}`)}}>
+        <img src={props.furnitures.img} width="80%"/>
         <h4>{props.furnitures.title}</h4>
         <p>{props.furnitures.content}</p>
       </Col>
