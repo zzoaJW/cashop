974a3ad02a0242a2c6fb7d08d4df7aaa2fea4f1c
z(0o0a) <i7can7i@gmail.com>
2023-04-02 21:51:03 +0900

    수량 범위 설정, 수량 입력값 예외 설정, MUI 적용

--- src/pages/detailPage.js
+++ src/pages/detailPage.js
@@ -2,6 +2,7 @@ import Button from 'react-bootstrap/Button';
 import { useEffect, useState } from "react";
 import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
+import TextField from '@mui/material/TextField';
 
 function DetailPage(props){
 
@@ -9,37 +10,52 @@ function DetailPage(props){
     let {id} = useParams();
     let findFurniuture = props.furnitures.find((e) => e.id == id);
 
-    let [txt, setTxt] = useState("")
-    let [count, setCount] = useState(0)
-    let [sale, setSale] = useState(true);
+    let [cnt, setCnt] = useState("")
+    let [discount, setDiscount] = useState(true)
+    let [btnDisable, setBtnDisable] = useState(true)
     useEffect(()=>{
         setTimeout(()=>{ 
-            setSale(false)
+            setDiscount(false)
         }, 2000)
 
-        if(isNaN(txt)){
-            alert('숫자만 쓰세요')
+        if(cnt<1){
+            setBtnDisable(true)
+        }else{
+            setBtnDisable(false)
         }
-        
-    }, [txt])
+    }, [cnt])
 
     return (
         <div className="container">
             {
-                sale? <div className='alert alert-warning'>2초 이내 구매시 할인</div> : null
+                discount? <div className='alert alert-warning'>2초 이내 구매시 할인</div> : null
             }
-            {count}
-            <button onClick={()=>{setCount(count+1)}}>버튼</button>
             <div className="row">
                 <div className="col-md-6">
                     <img src={findFurniuture.img} width="100%" />
                 </div>
-                <input placeholder='수량을 적어주세요' onChange={(e)=>{setTxt(e.target.value);}}/>
                 <div className="col-md-6">
                     <h4 className="pt-5">{findFurniuture.title}</h4>
                     <p>{findFurniuture.content}</p>
                     <p>{findFurniuture.price}</p>
-                    <Button variant="outline-success">주문하기</Button>{' '}
+                    <TextField label="수량" type="number" fullWidth="fullWidth" 
+                        onChange={(e)=>{
+                                if(isNaN(cnt)){
+                                    e.target.value = 0
+                                }else{
+                                    if(e.target.value < 0){
+                                        e.target.value = 0
+                                    }else{
+                                        setCnt(e.target.value)
+                                    }
+                                }
+                            }}/>
+                    <p/>
+                    <Button variant="outline-success" 
+                        disabled={btnDisable}
+                        onClick={()=>{
+                                alert('\'' + findFurniuture.title + '\' 상품 ' + cnt + '개 주문 완료')
+                            }}>주문하기</Button>
                 </div>
             </div>
         </div> 
