2f05faddb3aa396cdc505cae6d4c1fd08ddf0511
z(0o0a) <i7can7i@gmail.com>
2023-04-08 02:40:42 +0900

    Lottie 추가

--- src/pages/aboutPage.js
+++ src/pages/aboutPage.js
@@ -2,12 +2,15 @@ import '../App.css';
 import { useEffect, useState } from "react";
 import { useNavigate, Outlet } from "react-router-dom";
 import styled from 'styled-components';
+import Lottie from "lottie-react";
 
 import temp from "../imgs/mainbannerimg.jpg";
+import downArrow from "../lotties/black-arrow-down.json";
 
 let Title = styled.h1`
     color : #3b3b3b;
     height : 1000px;
+    margin : 50px;
     display : flex;
     justify-content: center;
     align-items : center;
@@ -25,7 +28,11 @@ function AboutPage(){
 
     return (
         <div style={{backgroundColor:'#f8f9fa'}} className={'anim_fade_start ' + animEnd}>
-            <div><Title>Shopping Mall for Sweet Furnitures !</Title></div>
+            <div>
+                <div style={{ height:'50px' }}/>
+                <Lottie style={{ height:'100px'}} animationData={downArrow} />
+                <Title>Shopping Mall for Sweet Furnitures !</Title>
+            </div>
             <div><Title>This project is simple React practice.</Title></div>
             <div><Title>with Redux, style-component, Bootstrap ...</Title></div>
             <div><Title>Made by z(0o0a)</Title></div>
