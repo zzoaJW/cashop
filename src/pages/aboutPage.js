15d4946d0fc6d07d4dd5e750ccd6fed70374cefd
z(0o0a) <i7can7i@gmail.com>
2023-04-08 02:13:27 +0900

    aboutPage 디자인 수정

--- src/pages/aboutPage.js
+++ src/pages/aboutPage.js
@@ -1,7 +1,17 @@
 import '../App.css';
 import { useEffect, useState } from "react";
-import { motion } from "framer-motion";
 import { useNavigate, Outlet } from "react-router-dom";
+import styled from 'styled-components';
+
+import temp from "../imgs/mainbannerimg.jpg";
+
+let Title = styled.h1`
+    color : #3b3b3b;
+    height : 1000px;
+    display : flex;
+    justify-content: center;
+    align-items : center;
+`
 
 function AboutPage(){
     let navigate = useNavigate();
@@ -14,10 +24,14 @@ function AboutPage(){
 
 
     return (
-        <div className={'container anim_fade_start ' + animEnd}>
-            <h4>Shopping Mall for Sweet Furnitures !</h4>
-            <button onClick={()=>{ navigate("/about/project")}}>project</button> <button onClick={()=>{ navigate("/about/member")}}>member</button>
-            <Outlet/>
+        <div style={{backgroundColor:'#f8f9fa'}} className={'anim_fade_start ' + animEnd}>
+            <div><Title>Shopping Mall for Sweet Furnitures !</Title></div>
+            <div><Title>This project is simple React practice.</Title></div>
+            <div><Title>with Redux, style-component, Bootstrap ...</Title></div>
+            <div><Title>Made by z(0o0a)</Title></div>
+            <div><img style={{width:'100%', marginBottom:'200px'}} src={temp}></img></div>
+            {/* <button onClick={()=>{ navigate("/about/project")}}>project</button> <button onClick={()=>{ navigate("/about/member")}}>member</button>
+            <Outlet/> */}
         </div>
     )
 }
