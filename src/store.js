60717ab1e4f10abfd3ed3f775ab5180b44ccd7a8
z(0o0a) <i7can7i@gmail.com>
2023-04-07 23:55:18 +0900

    redux 기본 사용법 - slice 생성 및 등록 방법

--- src/store.js
+++ src/store.js
@@ -1,13 +1,13 @@
 import { configureStore, createSlice } from '@reduxjs/toolkit'
 
 // slice == state
-let 변수명 = createSlice({
+let user = createSlice({
   name : 'state 이름',
   initialState : '값'
 })
 
 export default configureStore({
   reducer: { 
-    작명 : 변수명.reducer
+    user : user.reducer
   }
 })
