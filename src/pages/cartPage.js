eec8f5e204ccd6570be91ca8d532ce6da72a3eef
z(0o0a) <i7can7i@gmail.com>
2023-04-09 13:02:15 +0900

    Redux - array, object 변경함수, 파라미터 활용

--- src/pages/cartPage.js
+++ src/pages/cartPage.js
@@ -1,7 +1,8 @@
 import '../App.css';
 import { useEffect, useState } from "react";
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import styled, { keyframes } from 'styled-components';
+import { addAge, changeName, addCart, cleanCart, changeCount } from './../store.js';
 
 let Button = styled.button`
     background-color : white
@@ -16,11 +17,17 @@ function CartPage(){
     }, [])
 
     // store
-    let cart = useSelector((state)=>{ return state.cart })
+    let cart = useSelector( (state) => state.cart )
+    let user = useSelector( (state) => state.user )
+    let dispatch = useDispatch()
 
-
+    
     return (
         <div className={'container anim_fade_start ' + animEnd}>
+            <div>
+                <p style={{color:'#9DD84B' }}>{user.age}살 {user.name} 님</p>
+                <Button onClick={()=>{ dispatch(addAge(100)) }}>나이 먹기</Button> <Button onClick={()=>{ dispatch(changeName()) }}>이름 바꾸기 (1회)</Button>
+            </div>
             <table class="table">
                 <thead>
                     <tr>  {/* tr : 행 1개 생성*/}
@@ -35,24 +42,29 @@ function CartPage(){
                     {
                         cart.map(function(c, i){
                             return (
-                                <TableBody funiture={c}/>
+                                <TableBody funiture={c} dispatch={dispatch}/>
                             )
                         })
                     }
                 </tbody>
             </table>
+            <Button onClick={()=>{
+                dispatch(cleanCart())
+            }}>clean cart</Button>
         </div>
     )
 }
 
-function TableBody({funiture}){
+function TableBody({funiture, dispatch}){
     return (
         <tr style={{borderColor:'#ffffff'}}>
             <td>{funiture.id}</td>
             <td>{funiture.name}</td>
             <td>{funiture.count}</td>
             <td>{funiture.price*funiture.count}</td>
-            <td><Button>+</Button></td>
+            <td><Button onClick={()=>{
+                dispatch(changeCount(10))
+            }}>+</Button></td>
         </tr>
     )
 }
--- src/store.js
+++ src/store.js
@@ -1,16 +1,43 @@
 import { configureStore, createSlice } from '@reduxjs/toolkit'
 
-// slice == state
+let user = createSlice({
+  name : 'user',
+  initialState : { name : 'jenny', age : 20 },
+  reducers : {
+    addAge(state, a){
+      state.age += a.payload
+    },
+    changeName(state){
+      state.name = 'kai'
+    }
+  }
+})
+
+export let { addAge, changeName } = user.actions
 
 let cart = createSlice({
   name : 'cart',
   initialState : [ {id : 0, name : 'White and Black', count : 2, price : 23000}, 
-                    {id : 2, name : 'Grey Yordan', count : 1, price : 50000} ] 
+                    {id : 2, name : 'Grey Yordan', count : 1, price : 50000} ],
+  reducers : {
+      addCart(state){  // 장바구니 추가
+        return state + {}
+      },
+      cleanCart(){  // 장바구니 비우기
+        return []
+      },
+      changeCount(state, action){
+        state[0].count = action.payload
+      }
+  }
 })
+
+export let { addCart, cleanCart, changeCount } = cart.actions
 
 
 export default configureStore({
   reducer: { // slice 등록
-    cart : cart.reducer
+    cart : cart.reducer,
+    user : user.reducer
   }
 })
